33caa5df7f73ceb73fe18b9b82817c4c64a6a0d1
Andre Ortiz <andreortiz82@gmail.com>
2016-07-26 10:53:29 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="outer-container">
+      <div class="container">
+        <h1>{{ msg }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Sucka!'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "src/scss/asset-bundle";
+
+body {
+  font-family: Helvetica, sans-serif;
+  color: $blue;
+  @extend .bg-black;
+  padding:rem(20);
+}
+
+.outer-container {
+  @include outer-container($grid-fixed);
+
+  .container {
+    @include span-columns(12);
+    @include shadow;
+    border:rem(5) solid $light;
+    padding:rem(20);
+    @extend .bg-white;
+  }
+}
+</style>
